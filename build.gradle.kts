e03dffe7484eae5a2b994710e948161cb60bf862
ProfAttila7 <zdua0309@uni.sydney.edu.au>
2024-10-09 23:00:04 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,6 @@ buildscript {
 plugins {
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
